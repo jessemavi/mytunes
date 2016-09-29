c4046c94017ac018e76271e573ef97fd02534c95
jessemavi <jesse.mavi@gmail.com>
2016-09-28 18:59:41 -0700

    Implement Songs Collection

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -4,21 +4,28 @@ var Songs = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+
+    var context = this;
+
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/songs/',
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
-        console.log('success', data);
-        var library = new Songs(data);
-    
+        console.log('success');
+        context.populateSongs(data.results);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('failed to get songs', data);
       }
     });
+  },
+
+  populateSongs: function(songs) {
+    this.set(songs);
   }
+
 
 });
